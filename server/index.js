defc7077bdc9d64a81a59403dd91aa50b23bf6e1
Jeremy Lucas <jplucas14@gmail.com>
2021-08-04 15:42:50 -0600

    Renamed mongodb and moved to env after password change

--- server/index.js
+++ server/index.js
@@ -12,8 +12,7 @@ app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
-const CONNECTION_URL =
-  'mongodb+srv://memories-mern:memories-mern123@cluster0.0ubfg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose
